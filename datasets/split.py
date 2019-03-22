ef9e2cc17391e18c0da624d218ec7b71ee2e4f07
jsanch90 <jsanch90@eafit.edu.co>
2019-03-22 12:26:55 -0500

    split data

---
+++ datasets/split.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+import sys
+
+data = pd.read_csv(sys.argv[1])
+
+train, test = train_test_split(data, shuffle=True)
+ 
+train.to_csv('train.csv',index=False)
+test.to_csv('test.csv',index=False)
