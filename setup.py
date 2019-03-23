5dcd3b41e4e603d4aad161fca8d5632b11b08bc3
jsanch90 <jsanch90@eafit.edu.co>
2019-03-23 11:55:50 -0500

    fixing setup.py file

--- setup.py
+++ setup.py
@@ -1,4 +1,24 @@
-def setup():
-    print('Setup')
+from setuptools import setup, find_packages
+from pip.req import parse_requirements
+import re, ast
 
-setup()
+#get version from version variable in plaid_integration/init.py
+_version_re = re.compile(r'version\s+=\s+(.*)')
+
+with open('plaid_integration/init.py', 'rb') as f:
+    version = str(ast.literal_eval(_version_re.search(
+    f.read().decode('utf-8')).group(1)))
+
+requirements = parse_requirements('requirements.txt', session="")
+
+setup(
+name='plaid_integration',
+version=1,
+description='Plaid Integration',
+author='Vaico',
+author_email='pioneros@github.com',
+packages=find_packages(),
+zip_safe=False,
+include_package_data=True,
+dependency_links=[str(ir._link) for ir in requirements if ir._link]
+)
