9973bd4af2545600d3c3500652ec3b5e188d1b57
jsanch90 <jsanch90@eafit.edu.co>
2019-04-19 23:22:13 -0500

    Adding camera tests

---
+++ tests/cam_test.py
@@ -0,0 +1,20 @@
+import cv2
+import pytest
+
+@pytest.fixture()
+def test_cam_connect():
+    video_capture = cv2.VideoCapture(0)
+    #print(video_capture.isOpened())
+    return video_capture.isOpened()
+
+@pytest.fixture()
+def test_cam_not_connect():
+    video_capture = cv2.VideoCapture(2)
+    #print(video_capture.isOpened())
+    return video_capture.isOpened()
+
+def test_answer(test_cam_connect,test_cam_not_connect):
+    res = test_cam_connect
+    res2 = test_cam_not_connect
+    assert res == True
+    assert res2 == False
