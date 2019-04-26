5752f823f681e0671d0e79d2e4f33043a0760b80
jsanch90 <jsanch90@eafit.edu.co>
2019-04-25 19:22:49 -0500

    Attaching files to an email to send report

--- app/email_service.py
+++ app/email_service.py
@@ -1,5 +1,11 @@
 import smtplib, ssl
-from email.message import EmailMessage
+import cv2
+from imageio import imread
+import io
+import base64
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 class Email_services():
 
@@ -12,22 +18,43 @@ class Email_services():
     
     def get_sender(self):
         return self.sender_email
-
     
-    """Recipients must be a list with the email addresses that you want to send the message"""
-    def send_email(self,recipients,subject='Reporte de la obra',message="""Vaico Works email service is working"""):
-        msg = EmailMessage()
+    def get_image_from_base64(self,base64_str, out_name='b64img'):
+        out_name = out_name+'.jpg'
+        #b64_string = base64_str.decode()
+        img_temp = imread(io.BytesIO(base64.b64decode(base64_str)))
+        cv2_img = cv2.cvtColor(img_temp, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(out_name, cv2_img)
+
+    """
+    + Recipients must be a list with the email addresses that you want to send the message.
+    + Attachment parameter must be a string with the path of the image.
+    """
+    def send_email(self,recipients,subject='Reporte de la obra',
+                        message="Vaico Works email service is working",
+                        attachment=None):
+
+        msg = MIMEMultipart()
         msg['Subject'] = subject
-        msg['From'] = self.get_sender()
+        msg['From'] = 'vaicoworksreportes@gmail.com'
         msg['To'] = ', '.join(recipients)
-        msg.set_content(message)
+        body = MIMEText(message) # convert the body to a MIME compatible string
+        msg.attach(body)
+        msg.preamble = 'Vaico Works'
+
+        if attachment is not None:
+            filename = attachment.split('/')[-1]
+            with open(attachment,'rb') as _file:
+                img = MIMEImage(_file.read())
+                img.add_header('Content-Disposition', "attachment; filename= {0}".format(filename))
+            msg.attach(img)
 
         server = smtplib.SMTP(self.smtp_server,self.port)
 
         try:
             server.ehlo() 
             server.starttls(context=self.context) # Secure the connection
-            server.ehlo() # Can be omitted
+            server.ehlo()
             server.login(self.get_sender(), self.password)
             server.send_message(msg)
 
@@ -37,6 +64,7 @@ class Email_services():
         finally:
             server.quit()
         print('Message sent')
+    
 
-# x = Email_services()
-# x.send_email(['este991@gmail.com','p4rsek@gmail.com'])
+#x = Email_services()
+#x.send_email(['p4rsek@gmail.com','camilovilla699@gmail.com'],attachment='/home/josh/MEGA/Keras/test_vaico/person6.jpg')#
