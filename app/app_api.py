0f5aefce6fecc554835ebb6e7289facc00e43bf5
EC2 Default User <ec2-user@ip-172-31-91-27.ec2.internal>
2019-05-02 17:08:27 +0000

    Docker files

--- app/app_api.py
+++ app/app_api.py
@@ -261,4 +261,4 @@ def delete_specific_user(email):
     return redirect(url_for('delete_user'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',debug=True)
+    app.run(host='0.0.0.0',port=80 ,debug=True)
